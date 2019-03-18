fe6c200b7b04e9b98d39f234262f39b89d86dde9
Amila Welihinda <amilajack@gmail.com>
2019-03-17 22:03:45 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# dark-demo
+
+A small runnable demo of [Dark](https://github.com/amilajack/dark), a modern fork of commander.js
+
+## Setup
+
+```bash
+git clone https://github.com/amilajack/dark-demo
+cd dark-demo
+yarn
+node index.js
+```
+
+## Support
+
+Do you like this project? Here's how you can say thanks:
+
+* Star the project
+* [Follow me on Twitter](https://twitter.com/amilajack) for updates on my projects!
+* Support me on Patreon ❤️
+
+<p>
+  <a href="https://www.patreon.com/amilajack">
+    <img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width="160">
+  </a>
+</p>
+
+## Related
+
+* [**dark**](https://github.com/amilajack/dark/): A modern fork of commander.js
---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+const { default: dark } = require('dark');
+
+const program = dark()
+  .version('0.1.0')
+  .option('-p, --peppers', 'Add peppers')
+  .option('-P, --pineapple', 'Add pineapple')
+  .option('-b, --bbq-sauce', 'Add bbq sauce')
+  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .parse(process.argv);
+
+console.log('you ordered a pizza with:');
+if (program.peppers) console.log('  - peppers');
+if (program.pineapple) console.log('  - pineapple');
+if (program.bbqSauce) console.log('  - bbq');
+console.log('  - %s cheese', program.cheese);
+
+// How you call the API:
+//
+// node index.js -c
+// node index.js -c -p
+// node index.js --cheese --peppers
